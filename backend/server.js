fdeaf49da0e6499cc4c06824312caea57968c9bc
preranasinghh <preranasingh1176@gmail.com>
2023-07-02 18:29:34 +0530

    updating readme

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,13 @@ const Contact = require('./models/contactModel')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
